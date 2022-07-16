7265fb232daecc9b0d1319ab1177443bf0daf4eb
AleCostaS <alexandrec.silva@usp.br>
2022-07-16 16:57:52 -0300

    style e reset css colocados

---
+++ Área de Trabalho/projeto7-instagramreact/src/components/Menu.js
@@ -0,0 +1,38 @@
+export default function Menu() {
+    const logo = "./assets/img/logo.png";
+
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                    <div class="separador"></div>
+                    <img src={logo} />
+                </div>
+
+                <div className="logo-mobile">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                </div>
+
+                <div className="instagram-mobile">
+                    <img src={logo} />
+                </div>
+        
+                <div className="pesquisa">
+                    <input type="text" placeholder="Pesquisar" />
+                </div>
+        
+                <div className="icones">
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                    <ion-icon name="compass-outline"></ion-icon>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="person-outline"></ion-icon>
+                </div>
+
+                <div className="icones-mobile">
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                </div>
+            </div>
+        </div>
+	);
+}
--- Área de Trabalho/projeto7-instagramreact/src/index.js
+++ Área de Trabalho/projeto7-instagramreact/src/index.js
@@ -1,43 +1,8 @@
 import ReactDOM from 'react-dom';
+import Menu from './components/Menu';
 
-console.log('AQUI')
-
-//Componente
-function Menu() {
-	return (
-        <div class="navbar">
-            <div class="container">
-                <div class="logo">
-                    <ion-icon name="logo-instagram"></ion-icon>
-                    <div class="separador"></div>
-                    <img src="assets/img/logo.png" />
-                </div>
-
-                <div class="logo-mobile">
-                    <ion-icon name="logo-instagram"></ion-icon>
-                </div>
-
-                <div class="instagram-mobile">
-                    <img src="assets/img/logo.png" />
-                </div>
-        
-                <div class="pesquisa">
-                    <input type="text" placeholder="Pesquisar" />
-                </div>
-        
-                <div class="icones">
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                    <ion-icon name="compass-outline"></ion-icon>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="person-outline"></ion-icon>
-                </div>
-
-                <div class="icones-mobile">
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-            </div>
-        </div>
-	);
-}
+import "./css/reset.css";
+import "./css/style.css";
+import 'ionicons';
 
 ReactDOM.render(<Menu />, document.querySelector(".root"));
